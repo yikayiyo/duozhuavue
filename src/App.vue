6ce36c5f63376bfe525d2bdf247ef8f69339b5b2
yikayiyo <837937787@qq.com>
2022-02-19 16:40:19 +0800

    feat: add a simple dark mode toggle button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
-<script setup>
+<script>
+import { ref } from "vue";
 import Wrapper from "./components/Wrapper/Wrapper.vue";
+export default {
+	components: {
+		Wrapper,
+	},
+	setup() {
+		const isDark = ref(false);
+		function toggleMode() {
+			isDark.value = !isDark.value;
+		}
+		return {
+			isDark,
+			toggleMode,
+		};
+	},
+};
 </script>
 
 <template>
-	<div class="bg-gray-100 dark:bg-darkbg">
-		<wrapper />
+	<div :class="isDark ? 'dark' : ''">
+		<div class="bg-gray-100 dark:bg-darkbg">
+			<wrapper />
+			<div
+				class="
+					toggle
+					absolute
+					top-0
+					right-0
+					z-20
+					p-3
+					border-2
+					dark:border-menu dark:text-white
+				"
+				@click="toggleMode"
+			>
+				{{ isDark ? "亮" : "暗" }}
+			</div>
+		</div>
 	</div>
 </template>
 
